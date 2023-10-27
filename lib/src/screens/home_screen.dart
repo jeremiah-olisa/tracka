a1861523f2c2318141d6e8938495b23c39180a0e
Jeremiah Olisa <jeremiaholisa453@gmail.com>
2023-10-27 16:46:16 +0100

    home page

---
+++ lib/src/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Center(
+        child: Text("Home Page"),
+      ),
+    );
+  }
+}
